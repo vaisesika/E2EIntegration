8f0c922dd863f12488995110de8436d49ca4efdd
DFW <DFW@CACDTL03SV707Y.ITServices.sbc.com>
2018-08-12 00:56:56 -0700

    Created Automation Project

---
+++ SeleniumE2EJenkins/src/testcases/Testcase1.java
@@ -0,0 +1,28 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Testcase1 {
+	
+@Test
+
+public void testing()
+{
+	System.setProperty("webdriver.chrome.driver","./Driver/chromedriver.exe");
+	ChromeDriver driver = new ChromeDriver();
+	driver.get("http://www.facebook.com");
+	driver.findElementById("email").sendKeys("mentorkmnd");
+	driver.findElementById("pass").sendKeys("vaisesika");
+	driver.quit();
+}
+
+
+
+
+	
+	
+	
+	
+
+}
